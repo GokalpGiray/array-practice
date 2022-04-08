7e2ff8a7be90050875778e38fb4f20d30b0e7fe3
Gokalp Giray <gokalpgiray24@gmail.com>
2022-04-08 15:18:00 +0300

    GG-1 İlk Taslak Oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+const initialFormValue = { nameWork: "" };
+
 function App() {
+  const [work, setWork] = useState([{ nameWork: "" }]);
+  const [form, setForm] = useState(initialFormValue);
+
+  const handleOnChange = event => {
+    const taha = event.target;
+    setForm({ ...form, [taha.name]: taha.value });
+  };
+
+  useEffect(() => {
+    setForm(initialFormValue);
+  }, [work]);
+
+  const addWork = (e) => {
+    e.preventDefault();
+
+    if (form.nameWork === "") {
+      return false;
+    }
+
+    setWork([...work, form]);
+    console.log(work);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <form onSubmit={addWork}>
+            <input
+              type="text"
+              name='nameWork'
+              className="new-todo"
+              placeholder="What needs to be done?"
+              autofocus
+              value={form.nameWork}
+              onChange={handleOnChange}
+            />
+          </form>
+        </header>
+
+        <section className="main">
+          <input className="toggle-all" type="checkbox" />
+          <label htmlFor="toggle-all">
+            Mark all as complete
+          </label>
+
+          <ul className="todo-list">
+            <li className="completed">
+              <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>Learn JavaScript</label>
+                <button className="destroy"></button>
+              </div>
+            </li>
+            <li>
+              <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>Learn React</label>
+                <button className="destroy"></button>
+              </div>
+            </li>
+            <li>
+              <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>Have a life!</label>
+                <button className="destroy"></button>
+              </div>
+            </li>
+          </ul>
+        </section>
+
+        <footer className="footer">
+
+          <span className="todo-count">
+            <strong>2</strong>
+            items left
+          </span>
+
+          <ul className="filters">
+            <li>
+              <a className="selected">All</a>
+            </li>
+            <li>
+              <a>Active</a>
+            </li>
+            <li>
+              <a>Completed</a>
+            </li>
+          </ul>
+
+          <button className="clear-completed">
+            Clear completed
+          </button>
+        </footer>
+      </section>
+
+      <footer className="info">
+        <p>Click to edit a todo</p>
+        <p>Created by <a href="https://d12n.me/">Dmitry Sharabin</a></p>
+        <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+      </footer>
     </div>
   );
 }
